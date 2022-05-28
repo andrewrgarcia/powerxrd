119e01da515596a8a9cda127d6ee3e10d807455f
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-05-28 12:03:54 -0400

    troubleshoot

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='powerxrd',
-      version='0.0.2',
+      version='0.0.3',
       description='Simple tools to handle powder XRD (and XRD) data with Python',
       url='https://github.com/andrewrgarcia/powerxrd',
       author='Andrew Garcia, PhD',
