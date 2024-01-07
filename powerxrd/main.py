91ecdc43de0efe72533b65f2d2b77427c4ce033f
Andrew Garcia <garcia.gtr@gmail.com>
2024-01-06 19:55:24 -0500

    refinement_flags is introduced to keep track of which data points are marked for refinement.

--- powerxrd/main.py
+++ powerxrd/main.py
@@ -54,8 +54,21 @@ class Data:
             file name and/or path for XRD file in .xy format
         '''
         self.file = file
+        self.refinement_flags = None  # New attribute to store flags for refinement
 
-    
+
+    def set_refinement_flags(self, indices, flag):
+        if self.refinement_flags is None:
+            self.refinement_flags = [True] * len(self.x)  # Initialize all flags to True
+        for index in indices:
+            self.refinement_flags[index] = flag
+
+    def get_refinable_data(self):
+        if self.refinement_flags is None:
+            return self.x, self.y  # If no flags are set, all data is refinable
+        else:
+            return self.x[self.refinement_flags], self.y[self.refinement_flags]
+
     def importfile(self):
 
         if self.file.split(".")[1]=='xy':
